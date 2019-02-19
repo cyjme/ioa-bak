499b1bc7dcb0ac359bd98009ad098757a00f4ef1
jason <changyuanjian@gmail.com>
2019-02-19 20:00:43 +0800

    update docker
    Former-commit-id: 658e7aaf777f125401fdec41999e2a23a97c12b2
    Former-commit-id: 4c2cfde8f28336b46aad6d94937c0dadc26d3359

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"flag"
 	"ioa"
 	"ioa/store"
 )
 
 func main() {
-	store.Init()
 	config := ioa.ReadConfig()
+
+	flag.StringVar(&config.Proxy.Host, "host", "0.0.0.0", "")
+	flag.StringVar(&config.Proxy.Port, "port", "9992", "")
+	flag.IntVar(&config.Proxy.MaxIdleConns, "maxIdleConns", 10000, "")
+	flag.IntVar(&config.Proxy.MaxIdleConnsPerHost, "maxIdleConnsPerHost", 10000, "")
+	flag.Parse()
+
+	store.Init()
 	Ioa := ioa.New(config)
 	Ioa.StartServer()
 }
